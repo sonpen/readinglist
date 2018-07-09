38b57aeff91039aca2358ca95056c4b1cb660617
sonpen <sopnen76@naver.com>
2018-07-09 23:10:05 +0900

    3.1.2 사용자 정의 보안 구성하기

---
+++ src/main/java/com/manning/ReaderRepository.java
@@ -0,0 +1,9 @@
+package com.manning;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 1109806 on 2018-07-09.
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+}
---
+++ src/main/java/com/manning/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.manning;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by 1109806 on 2018-07-09.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private ReaderRepository readerRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").access("hasRole('READER')")   // READER 권한 필요
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")    // 로그인 폼 경로 설정
+                .failureUrl("/login?error=true");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() {   // 사용자 정의 UserDetailService
+            @Override
+            public UserDetails loadUserByUsername(String username)
+                throws UsernameNotFoundException {
+                return readerRepository.findOne(username);
+            }
+
+        });
+    }
+}
